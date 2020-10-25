f234e9509064c61fd6f159fac21a57227f2270c5
Kwynn <buessresume@gmail.com>
2020-10-24 20:16:27 -0400

    successful parsing of GitHub API results

--- getActual.php
+++ getActual.php
@@ -5,7 +5,7 @@ require_once('/opt/kwynn/kwutils.php');
 class github_api_user_repo_get {
     
     const url = 'https://api.github.com/users/kwynncom/repos';
-    const iterLimit = 4; // This probably limits to 120 repos, but I want it here for now to prevent infinite loops
+    const iterLimit = 6; // This limits the number of repos, but I want it here for now to prevent infinite loops
     
     
     public static function get() {
@@ -13,18 +13,21 @@ class github_api_user_repo_get {
 	return $o->getI();
     }
     
-    private function __construct() {
-	$this->get10();
-    }
+    private function __construct() { $this->get10();  }
+    
+    public function getI() { return $this->rawa; }
     
     private function get10() {
 
 	$i = 0;
 	$this->rawa = [];
+	$url = self::url;
 	do {
-	    // $htr = $this->getActualActual($sinceid);
-	    $this->p10(file_get_contents('/tmp/git'));
-	} while ($i++ < -1); // ** TESTING
+	    $htr = $this->getActualActual($url);
+	    $nu = $this->p10($htr);
+	    if (!$nu) return;
+	    $url = $nu;
+	} while ($i++ < self::iterLimit);
     }
     
     private function p10($rin) {
@@ -33,19 +36,14 @@ class github_api_user_repo_get {
 	if ($ta) $this->rawa = array_merge($this->rawa, $ta);
 	$h = $hba[0];
 	// link: <https://api.github.com/user/14192685/repos?page=2>; rel="next", <https://api.github.com/user/14192685/repos?page=2>; rel="last"
-	$mr = preg_match('/\blink\: <([^>]+)>; rel="next", <([^>]+)>; rel="last"/', $h, $m);
-	if (!$mr) return;
-	
-	return;
+	$mr = preg_match('/\blink\: <([^>]+)>; rel="next", <([^>]+)>; rel="last"/', $h, $ma);
+	if (!$mr) return false;
+	return $ma[1];
 		
     }
     
-    private function getActualActual() {
+    private function getActualActual($url) {
 	$ch = curl_init();
-	
-	$url = self::url;
-	// if ($sinceid) $url .= '?since=' . $sinceid;
-	
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	$h = ['User-Agent: curl/PHP'];
