756fcc22cf56a6031ec0ed4c9847b958b3fb7430
kwynncom <buessresume@gmail.com>
2023-02-19 00:03:16 -0500

    removed the summary table; individual rows, pushed_at rather than updated_at

--- get.php
+++ get.php
@@ -3,75 +3,44 @@
 require_once('/opt/kwynn/kwutils.php');
 require_once('getActual.php');
 
-class GitGetAct extends dao_generic {
+class GitGetAct extends dao_generic_3 {
     
-    const db = 'repos';
+    const dbname = 'repos';
     const quotaMinutes = 15;
-    
-    public function __construct()  {
-	parent::__construct(self::db);
-	$this->lcoll = $this->client->selectCollection(self::db, 'latest');
-	$this->scoll = $this->client->selectCollection(self::db, 'snapshot');
 	
-	$this->regGet();
-	$this->p10();
-	$this->p20();
-	$this->save();
-	return;
-    }
+	const uaf =   'pushed_at';
+	const gfs =  [self::uaf, 'html_url', /* 'description' , 'id', 'node_id', 'created_at' */];
 
-    private function save() {
-	if (isset($this->suma)) return;
-	$q = ['latest' => true];
-	$a = $this->thea[0];
-	$d = [];
-	$d['cnt'] = count($this->thea);
-	$d['asof'] = $this->asof;
-	$d['asr' ] = date('r', $d['asof']);
-	$d['source'] = $this->source;
-	$la = array_merge($a, $q, $d);
-	$this->lcoll->upsert($q, $la);
-	$sa = array_merge($d, ['rs' => $this->thea]);
-	$this->scoll->insertOne($sa);
-	$this->suma = $d;
+   
+    public function __construct()  {
+		parent::__construct(self::dbname);
+		self::creTabs('repos');
+
+		$this->regGet();
+		$this->p10();
+		$this->save();
+		return;
     }
-    
-    private function sort($a, $b) { return $b['ts'] - $a['ts'];   }
-    
-    private function p20() { 
-	if (!isset($this->thea)) return;
-	usort($this->thea, [$this, 'sort']);    
-    }
-       
+	
+	private function save() { foreach($this->thea as $r) $this->rcoll->upsert(['_id' => $r['_id']], $r); }
+
     private function p10() {
-	
-	if (isset($this->suma)) return;
-	
-	$fs = ['updated_at', 'html_url', 'description', 'id', 'node_id', 'created_at' ];
-	
-	$r = [];
-	foreach($this->rawa as $w) {
-	    $t = [];
-	    foreach($fs as $f) $t[$f] = $w[$f];
-	    $t['ts'] = strtotime($w['updated_at']);
-	    $r[] = $t;
-	}
-	
-	$this->thea = $r; unset($this->htj);
+
+
+		$r = [];
+		foreach($this->rawa as $w) {
+			$t = [];
+			foreach(self::gfs as $f) $t[$f] = $w[$f];
+			$t[self::uaf . '_U'] = strtotime($w[self::uaf]);
+			$t['_id'] = basename($w['html_url']);
+			$r[] = $t;
+		}
+
+		$this->thea = $r;
     }
     
     private function regGet() {
-	$since = time() - self::quotaMinutes * 60;
-	$r = $this->lcoll->findOne(['asof' => ['$gte' => $since]]);
-	if ($r) { 
-	    $this->suma = $r; 	
-	    $this->source = 'dbcache';
-	    return; 
-	}
-	
-	$this->source = 'curl';	
-	$this->rawa = github_api_user_repo_get::get();
-	$this->asof = time();
+		$this->rawa = github_api_user_repo_get::get();
     }
 }
 
--- getActual.php
+++ getActual.php
@@ -5,12 +5,11 @@ require_once('/opt/kwynn/kwutils.php');
 class github_api_user_repo_get {
     
     const url = 'https://api.github.com/users/kwynncom/repos';
-    const iterLimit = 6; // This limits the number of repos, but I want it here for now to prevent infinite loops
-    
+    const iterLimit = 12; // This limits the number of repos, but I want it here for now to prevent infinite loops
     
     public static function get() {
-	$o = new self();
-	return $o->getI();
+		$o = new self();
+		return $o->getI();
     }
     
     private function __construct() { $this->get10();  }
@@ -19,41 +18,41 @@ class github_api_user_repo_get {
     
     private function get10() {
 
-	$i = 0;
-	$this->rawa = [];
-	$url = self::url;
-	do {
-	    $htr = $this->getActualActual($url);
-	    $nu = $this->p10($htr);
-	    if (!$nu) return;
-	    $url = $nu;
-	} while ($i++ < self::iterLimit);
+		$i = 0;
+		$this->rawa = [];
+		$url = self::url;
+		do {
+			$htr = $this->getActualActual($url);
+			$nu = $this->p10($htr);
+			if (!$nu) return;
+			$url = $nu;
+		} while ($i++ < self::iterLimit);
     }
     
     private function p10($rin) {
-	$hba = explode("\r\n\r\n", $rin);
-	$ta = json_decode($hba[1], 1);
-	if ($ta) $this->rawa = array_merge($this->rawa, $ta);
-	$h = $hba[0];
-	// link: <https://api.github.com/user/14192685/repos?page=2>; rel="next", <https://api.github.com/user/14192685/repos?page=2>; rel="last"
-	$mr = preg_match('/\blink\: <([^>]+)>; rel="next", <([^>]+)>; rel="last"/', $h, $ma);
-	if (!$mr) return false;
-	return $ma[1];
+		$hba = explode("\r\n\r\n", $rin);
+		$ta = json_decode($hba[1], 1);
+		if ($ta) $this->rawa = array_merge($this->rawa, $ta);
+		$h = $hba[0];
+		// link: <https://api.github.com/user/14192685/repos?page=2>; rel="next", <https://api.github.com/user/14192685/repos?page=2>; rel="last"
+		$mr = preg_match('/\blink\: <([^>]+)>; rel="next", <([^>]+)>; rel="last"/', $h, $ma);
+		if (!$mr) return false;
+		return $ma[1];
 		
     }
     
     private function getActualActual($url) {
-	$ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	$h = ['User-Agent: curl/PHP'];
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $h);
-	curl_setopt($ch, CURLOPT_HEADER, 1);
-	$htr = curl_exec($ch);
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$h = ['User-Agent: curl/PHP'];
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $h);
+		curl_setopt($ch, CURLOPT_HEADER, 1);
+		$htr = curl_exec($ch);
 
-	file_put_contents('/tmp/git', $htr);
-	
-	return $htr;	
+		file_put_contents('/tmp/git', $htr);
+
+		return $htr;	
     }
 }
 
